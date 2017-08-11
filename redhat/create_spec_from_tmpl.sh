4ddf6f160323bc7c5d0edfc0c43055780a04d91a
Alan Hinchliff <hincha@renci.org>
2017-08-11 12:43:02 -0400

    using software release versions in rpm naming

--- redhat/create_spec_from_tmpl.sh
+++ redhat/create_spec_from_tmpl.sh
@@ -2,9 +2,12 @@
 DATE=`date "+%Y%m%d%H%M"`
 COMMIT=`git rev-parse HEAD`
 SHORTCOMMIT=`git rev-parse --short=8 HEAD`
+REL_VERSION=`git describe --tags --long --dirty --always | sed s/oess-plugin-// | sed s/-/_/g`
 
 cp na2-oess-plugin.spec.tmpl na2-oess-plugin.spec
 
 sed -i -e "s;@@DATE@@;${DATE};" na2-oess-plugin.spec
 sed -i -e "s;@@COMMIT@@;${COMMIT};" na2-oess-plugin.spec
-sed -i -e "s;@@SHORTCOMMIT@@;${SHORTCOMMIT};" na2-oess-plugin.spec
+#sed -i -e "s;@@SHORTCOMMIT@@;${SHORTCOMMIT};" na2-oess-plugin.spec
+sed -i -e "s;@@VERSION@@;${REL_VERSION};" nodeagent2.spec
+
