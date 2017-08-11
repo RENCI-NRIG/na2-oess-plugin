e6e47503c0392d0b98ca40201b88e60a3bb77900
mcevik0 <mcevik@renci.org>
2017-08-11 14:14:05 -0400

    simple version added

--- redhat/create_spec_from_tmpl.sh
+++ redhat/create_spec_from_tmpl.sh
@@ -3,11 +3,12 @@ DATE=`date "+%Y%m%d%H%M"`
 COMMIT=`git rev-parse HEAD`
 SHORTCOMMIT=`git rev-parse --short=8 HEAD`
 REL_VERSION=`git describe --tags --long --dirty --always | sed s/oess-plugin-// | sed s/-/_/g`
+SIMPLE_VERSION=`git describe --tags | sed s/oess-plugin-//`
 
 cp na2-oess-plugin.spec.tmpl na2-oess-plugin.spec
 
 sed -i -e "s;@@DATE@@;${DATE};" na2-oess-plugin.spec
 sed -i -e "s;@@COMMIT@@;${COMMIT};" na2-oess-plugin.spec
-#sed -i -e "s;@@SHORTCOMMIT@@;${SHORTCOMMIT};" na2-oess-plugin.spec
 sed -i -e "s;@@VERSION@@;${REL_VERSION};" na2-oess-plugin.spec
+sed -i -e "s;@@SIMPLE_VERSION@@;${SIMPLE_VERSION};" na2-oess-plugin.spec
 
